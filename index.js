62a724dd91c4dda97daccf8a941138ded2e74220
truongmanhdung04 <truongmanhdung04@gmail.com>
2021-04-28 23:17:32 +0700

    bài 2: sử dụng pug.js

--- index.js
+++ index.js
@@ -2,9 +2,23 @@ var express = require('express');
 var app = express();
 
 var port = 3000;
-app.get('/user', function(request, response){
-    response.send("Đây là trang user");
+app.set('view engine', 'pug')
+app.set('views', './views')
+app.get('/', function(request, response){
+    response.render('index',{
+        name: 'AAA',
+    });
 });
+
+app.get('/users',function(request,response){
+    response.render('users/index',{
+        users: [
+            { id: 1, name: 'dũng'},
+            { id: 2, name: 'hoàng'}
+        ]
+    });
+})
+
 app.listen(port, function (){
     console.log('Server listening on port ' + port);
 });
