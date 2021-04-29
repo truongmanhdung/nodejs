cf4802a48064e49b3293167de784999f399e826b
truongmanhdung04 <truongmanhdung04@gmail.com>
2021-04-29 10:04:08 +0700

    bài 3,4: get post methor lấy req và in ra res

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var app = express();
-
+var bodyParser = require('body-parser');
 var port = 3000;
+var users= [
+    { id: 1, name: 'dũng'},
+    { id: 2, name: 'hoàng'},
+    { id: 3, name: 'manh'},
+    { id: 4, name: 'anh'},
+    { id: 5, name: 'huy'},
+    { id: 6, name: 'quốc'},
+    { id: 7, name: 'linh'},
+    { id: 8, name: 'nam'},
+];
 app.set('view engine', 'pug')
 app.set('views', './views')
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 app.get('/', function(request, response){
     response.render('index',{
         name: 'AAA',
@@ -12,11 +24,27 @@ app.get('/', function(request, response){
 
 app.get('/users',function(request,response){
     response.render('users/index',{
-        users: [
-            { id: 1, name: 'dũng'},
-            { id: 2, name: 'hoàng'}
-        ]
+        users: users
     });
+});
+
+app.get('/users/search',function(request,response){
+    var q = request.query.q;
+    var matchedUsers = users.filter(function(user){
+        return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
+    });
+    response.render('users/index',{
+        users: matchedUsers
+    });
+});
+
+app.get('/users/create',function(request,response){
+    response.render('users/create',{
+    });
+    app.post('/users/create',function(request,response){
+        users.push(request.body);
+        response.redirect("./")
+    })
 })
 
 app.listen(port, function (){
