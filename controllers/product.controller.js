08cd70e9f41fb57ef97e6d8cd85d0fa14a795856
truongmanhdung04 <truongmanhdung04@gmail.com>
2021-05-14 22:51:56 +0700

    mongoose db: connect buoc dau

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,18 +1,24 @@
-var db = require('../db');
-var shortid = require('shortid');
-var md5 = require('md5');   
-module.exports.products = function(request,response){
-    // var drop = (page - 1) * perPage;
-    var page = parseInt(request.query.page) || 1; //nếu ko có gán gt page = 1
-    var perPage = 8;
-    var start = (page - 1) * perPage;
-    var end = page * perPage;
-    response.render('products',{
-        // cách 1
-        products: db.get('products').value().slice(start, end)
+// var db = require('../db');
+// var shortid = require('shortid');
+// var md5 = require('md5');   
 
-        //cách 2
-        //products: db.get('products').drop(drop).take(perPage).value()
+var Product = require('../models/product.model')
+module.exports.index = function(request,response){
+    // var drop = (page - 1) * perPage;
+    // var page = parseInt(request.query.page) || 1; //nếu ko có gán gt page = 1
+    // var perPage = 8;
+    // var start = (page - 1) * perPage;
+    // var end = page * perPage;
+    // response.render('products',{
+    //     // cách 1
+    //     products: db.get('products').value().slice(start, end)
+
+    //     //cách 2
+    //     //products: db.get('products').drop(drop).take(perPage).value()
+    // });
+    Product.find().then(function(products){
+        response.render('products/index',{
+            products: products
+        })
     });
-    
 }
--- index.js
+++ index.js
@@ -4,6 +4,10 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var csurf = require('csurf');
+var mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URL);
+
 
 var userRoute = require('./routes/user.router');
 var authMiddleware = require('./middlewares/auth.middleware');
